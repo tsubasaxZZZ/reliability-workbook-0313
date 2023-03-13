2cfe554de6fa4438ad09e33983e78ff27e4d2eb9
Tsubasa Nomura <tsubasa@nomupro.com>
2023-03-13 08:55:41 +0000

    Add Makefile to build

--- build/main.tf
+++ build/main.tf
@@ -193,21 +193,3 @@ resource "azurerm_application_insights_workbook" "example" {
   data_json           = local.workbook_data_json_for_community
 }
 
-// Generate workbook JSON
-resource "local_file" "workbook" {
-  count = var.deploy_community_edition_to_azure ? 1 : 0
-
-  filename = "${path.module}/ReliabilityWorkbook.workbook"
-  content  = local.workbook_data_json_for_community
-}
-
-resource "local_file" "workbook_public" {
-  filename = "${path.module}/ReliabilityWorkbookPublic.workbook"
-  content  = local.workbook_data_json_for_public
-}
-
-// Generate armtemplate JSON
-resource "local_file" "armtemplate" {
-  filename = "${path.module}/azuredeploy.json"
-  content  = local.armtemplate_json
-}
